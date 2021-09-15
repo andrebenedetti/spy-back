d68e51a178fe902f5db40d5dcda13444c051c100
drei <andredbenedetti@gmail.com>
2021-09-15 11:33:00 -0300

    Changes to cors policy

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const port = 80
 
 app.use(cors())
 
-app.post('/', (req, res) => {
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'lista-de-surpresas-2.herokuapp.com/');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    next();
+}
+
+app.post('/', allowCrossDomain, (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   console.log(ip); // ip address of the user
   res.json({
