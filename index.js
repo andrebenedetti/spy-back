b60ae1b460ca0553b9ff88f5b7792c782a59ef84
drei <andredbenedetti@gmail.com>
2021-09-15 11:22:34 -0300

    Change port to 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 80
 
 app.post('/', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
